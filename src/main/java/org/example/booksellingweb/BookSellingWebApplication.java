7a5cd634fa886d6750ae239121fee30cdf0ed055
kerian <james342506@gmail.com>
2025-05-23 16:58:51 +0630

    first

---
+++ src/main/java/org/example/booksellingweb/BookSellingWebApplication.java
@@ -0,0 +1,13 @@
+package org.example.booksellingweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookSellingWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookSellingWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/booksellingweb/BookSellingWebApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.booksellingweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookSellingWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
